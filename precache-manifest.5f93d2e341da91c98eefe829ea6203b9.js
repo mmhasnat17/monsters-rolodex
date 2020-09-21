0689f74bfa07f50b53350a94088778ad0129424e
mmhasnat17 <hasnat250@gmail.com>
2020-09-21 04:46:30 -0500

    Updates

---
+++ precache-manifest.5f93d2e341da91c98eefe829ea6203b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31cc057c318ad3eeb7e224a230cadc62",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "94cab3e8d206554419c8",
+    "url": "/monsters-rolodex/static/css/main.2592bead.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94cab3e8d206554419c8",
+    "url": "/monsters-rolodex/static/js/main.79d3c6cd.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
